9cce48813f21cd8cc93c3fb5ffe265b3ac917f31
protect333@bk.ru <protect333@bk.ru>
2022-12-05 18:23:38 +0300

    7

--- src/main/java/ru/spring/repisitory/ProductRepository.java
+++ src/main/java/ru/spring/repisitory/ProductRepository.java
@@ -4,7 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.spring.model.Product;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findAllByCostBetween(Double minCost, Double maxCost);
+    List<Product> findAllByCostIsGreaterThanEqual(Double minCost);
+    List<Product> findAllByCostIsLessThanEqual(Double maxCost);
 
 }
